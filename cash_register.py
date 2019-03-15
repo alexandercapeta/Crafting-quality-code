37fb643b75831e115e4899447063faa4514d21a5
Alexander Rivera <alexandercapeta@gmail.com>
2019-03-15 17:06:38 -0600

    A cash register that takes in currency values.
    Currency values are recognized as Canadian currency values.

---
+++ cash_register.py
@@ -0,0 +1,112 @@
+class CashRegister(object):
+    """A cash register."""
+
+    def __init__(self, loonies, toonies, fives, tens, twenties):
+        """
+        >>> register = CashRegister(5,5,5,5,5)
+        5
+        >>> register.loonies
+        5
+        >>> register.toonies
+        5
+        >>> register.fives
+        5
+        >>> register.tens
+        5
+        >>> register.twenties
+        5
+        """
+        self.loonies    = loonies
+        self.toonies    = toonies
+        self.fives      = fives
+        self.tens       = tens
+        self.twenties   = twenties
+
+    def __eq__(self, other):
+        """(CashRegister, CashRegister) -> bool
+        Return True if this CashRegister has the same amount of money as other.
+
+        >>> reg1 = CashRegister(2,0,0,0,0)
+        >>> reg2 = CashRegister(0,1,0,0,0)
+        >>> reg1 == reg2
+        True
+        """
+
+        return self.get_total() == other.get_total()
+
+    def get_total(self):
+        """(CashRegister) -> int
+        >>> register = CashRegister(5,5,5,5,5)
+        >>> register.get_total()
+        190
+        """
+        return self.loonies + self.toonies * 2 + self.fives * 5 \
+                + self.tens * 10 + self.twenties * 20
+
+    def add(self, count, denomination):
+        """(CashRegister, int, str) -> NoneType
+
+        Add count items of denomination to the register. Denominations must
+        be on of 'loonies', 'toonies', 'fives', 'tens', and 'twenties'.
+
+        >>> register = CashRegister(5,5,5,5,5)
+        >>> register.add(2, 'toonies')
+        >>> register.toonies
+        7
+        >>> register.add(1, 'tens')
+        >>> register.tens
+        6
+        """
+
+        if denomination == 'loonies':
+            self.loonies += count
+        elif denomination == 'toonies':
+            self.toonies += count
+        elif denomination == 'fives':
+            self.fives += count
+        elif denomination == 'tens':
+            self.tens += count
+        elif denomination == 'twenties':
+            self.twenties += count
+    def remove(self, count, denomination):
+        """(CashRegister, int, str) -> NoneType
+
+        Add count items of denomination to the register. Denominations must
+        be on of 'loonies', 'toonies', 'fives', 'tens', and 'twenties'.
+
+        >>> register = CashRegister(5,5,5,5,5)
+        >>> register.remove(2, 'toonies')
+        >>> register.toonies
+        3
+        >>> register.remove(1, 'tens')
+        >>> register.tens
+        4
+        """
+
+        if denomination == 'loonies':
+            self.loonies -= count
+        elif denomination == 'toonies':
+            self.toonies -= count
+        elif denomination == 'fives':
+            self.fives -= count
+        elif denomination == 'tens':
+            self.tens -= count
+        elif denomination == 'twenties':
+            self.twenties -= count
+    
+
+if __name__ == '__main__':
+##    # A cash register with 5 loonies, 5 toonies, 5 fives, 5 tens and 5 twenties
+##    #for a total of $190
+##    register = CashRegister(5, 5, 5, 5, 5)
+##    print(register.get_total())
+##
+##    register.add(3, 'toonies')
+##    register.remove(2, 'twenties')
+##
+##    print(register.get_total())
+    cr1 = CashRegister(2,0,0,0,0)
+    cr2 = CashRegister(0,1,0,0,0)
+    cr3 = CashRegister(1,1,0,0,0)
+    print(cr1 == cr2)
+    print(cr3 == cr2)
